4b1c01099f4f3dbdc51686ca4bd244fd0fde401e
Timo Scheuer <timo.scheuer@mail.de>
2025-07-10 22:45:59 +0200

    cleaned up dependencies and introduced modules

---
+++ core/core-citation/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.openimaj.citation {
+	requires java.compiler;
+	requires java.instrument;
+	requires jbibtex;
+	requires org.javassist;
+	requires org.apache.logging.log4j;
+	requires org.openimaj.aop;
+}
