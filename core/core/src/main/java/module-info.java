18106b8cc57f2436630d4ccec448debbb70a7cf4
Timo Scheuer <timo.scheuer@mail.de>
2025-07-13 22:11:16 +0200

    use modules

---
+++ core/core/src/main/java/module-info.java
@@ -0,0 +1,22 @@
+module org.openimaj.core {
+	requires java.prefs;
+	
+	requires ant;
+	requires colt;
+	requires com.esotericsoftware.kryo;
+	requires core;
+	requires jal;
+	requires org.apache.commons.io;
+	requires org.apache.commons.lang3;
+	requires org.apache.commons.text;
+	requires org.apache.commons.vfs2;
+	requires org.apache.httpcomponents.httpclient;
+	requires org.apache.httpcomponents.httpcore;
+	requires org.apache.logging.log4j;
+	requires org.apache.logging.log4j.core;
+	requires org.jsoup;
+	requires org.objenesis;
+	requires org.openimaj.citation;
+	
+	//exports ;
+}
